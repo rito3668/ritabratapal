2acd011df5fe4435c6d28ed8ad3ebfc0515a3d42
Ritabrata Pal <rito3668@gmail.com>
2025-01-17 12:14:19 +0530

    new year commit

--- src/Resume.js
+++ src/Resume.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import NITM from './assets/NITM.jpg'
-import Chat from './assets/chat.png'
+import Img1 from './assets/ppp.png'
 import Project from './assets/project.png'
 import Gemini from './assets/gemini.png'
 import { FaExternalLinkAlt } from "react-icons/fa";
@@ -22,6 +22,15 @@ export default function Resume() {
         {/* projects */}
         <h1 className='text-[28px] font-normal mt-20 text-center md:text-left '>Some Projects</h1>
         <div className='mt-10 '>
+        <div className='mt-8 max-w-[1000px] mx-auto block text-center md:text-left md:flex gap-[150px] items-start'>
+                <img src={Img1} alt="logo" className='w-[350px] h-[174px] lg:w-[400px] lg:h-[200px] mx-auto' />
+                <div className='mt-0'>
+                    <h1 className='text-xl font-extrabold my-3'>Imagify App</h1>
+                    <h2 className='font-normal'><span className='font-semibold'>Desc:</span> Fullstack Ai SAAS app to generate images from text prompt</h2>
+                    <h2 className='font-normal'><span className='font-semibold mr-2'>Techstack</span>React Js, Node Js, MongoDB </h2>
+                    <a target='_blank' href="https://github.com/rito3668/imagify" className='md:flex items-center gap-1 hover:text-blue-700 inline-flex mx-auto '>Github Repo <FaExternalLinkAlt size={15}/></a>
+                </div>
+            </div>
             <div className='mt-8 max-w-[1000px] mx-auto block text-center md:text-left md:flex gap-[150px] items-start'>
                 <img src={Project} alt="logo" className='w-[350px] h-[174px] lg:w-[400px] lg:h-[200px] mx-auto' />
                 <div className='mt-0'>
@@ -31,15 +40,7 @@ export default function Resume() {
                     <a target='_blank' href="https://github.com/rito3668/project-management" className='md:flex items-center gap-1 hover:text-blue-700 inline-flex mx-auto '>Github Repo <FaExternalLinkAlt size={15}/></a>
                 </div>
             </div>
-            <div className='mt-8 max-w-[1000px] mx-auto block text-center md:text-left md:flex gap-[150px] items-start'>
-                <img src={Gemini} alt="logo" className='w-[350px] h-[174px] lg:w-[400px] lg:h-[200px] mx-auto' />
-                <div className='mt-0'>
-                    <h1 className='text-xl font-extrabold my-3'>GEMINI CLONE</h1>
-                    <h2 className='font-normal'><span className='font-semibold'>Desc:</span> Web Interface for Chat Assistant using Gemini APIs</h2>
-                    <h2 className='font-normal'><span className='font-semibold mr-2'>Techstack</span>React Js, Gemini APIs</h2>
-                    <a target='_blank' href="https://github.com/rito3668/GeminiApp" className='md:flex items-center gap-1 hover:text-blue-700 inline-flex mx-auto '>Github Repo <FaExternalLinkAlt size={15}/></a>
-                </div>
-            </div>
+           
         </div>
         <div className='text-center font-light mt-20 mb-3 text-gray-600 text-[17px]'>Inspired by dank memes and big dreams :&#41; </div>
       <div className='text-center text-gray-600 font-light  text-[18px]'>	&#169; Ritabrata Pal 2024</div>
